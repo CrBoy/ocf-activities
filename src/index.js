4d845240341579ae0406e48fe8cdb4637fabcab1
CrBoy <crboy@crboy.net>
2018-08-24 15:54:29 +0800

    Convert json, which is from JSON_URI, to yaml, and save to dist/events.yml

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const fetch = require('node-fetch')
+const fs = require('fs')
+const yaml = require('js-yaml')
+
+const json_uri = process.env['JSON_URI'] || 'https://script.google.com/macros/s/AKfycbw0k4DJv039QNOLGGFpE7i_6nDul3wtZHQK_whJTvaWF7Awqd3F/exec'
+
+fs.mkdirSync('../dist')
+
+fetch(json_uri)
+  .then(res => res.json())
+  .then(json => { fs.writeFileSync('../dist/events.yml', yaml.dump(json)) } )
