af09a28fceb2484f513c1b2c867faa391de37c4d
CrBoy <crboy@crboy.net>
2018-09-26 17:30:37 +0800

    Move logic from gs to js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,44 @@ const fetch = require('node-fetch')
 const fs = require('fs')
 const yaml = require('js-yaml')
 
-const json_uri = process.env['JSON_URI'] || 'https://script.google.com/macros/s/AKfycbw0k4DJv039QNOLGGFpE7i_6nDul3wtZHQK_whJTvaWF7Awqd3F/exec'
+const json_uri = process.env['JSON_URI'] || 'https://script.google.com/macros/s/AKfycbxfJ4DluGcfuUyw0ed5FsU8Wvq-925l6R2HbeZ2hx7-5bOiPXOh/exec'
 
 try {
   fs.mkdirSync('../dist')
 } catch (e) {
 }
 
+function transformer (data) {
+  data = data.filter(function(row){
+    return row.info_check
+  }).reduce(function(acc, row){
+    acc[row.event_id] = row
+    delete row['event_id']
+    delete row['upload']
+    delete row['info_check']
+    Object.keys(row).forEach(function(key){
+      if(!row[key]) row[key] = null
+      if(key.match(/_id$/)) row[key] = semicolon_separated_list(row[key])
+      if(key === 'start' || key === 'end') row[key] = readableDate(row[key])
+    })
+    return acc
+  }, {})
+
+  return data
+}
+
+function semicolon_separated_list(str){
+  if(!str) return str
+  return str.split(';').filter(function(s){ return s }).map(function(s){ return s.trim() })
+}
+
+function readableDate(datetime) {
+  if(!datetime) return datetime
+  let d = new Date(datetime)
+  return `${d.getFullYear()}/${d.getMonth() + 1}/${d.getDate()}`
+}
+
 fetch(json_uri)
   .then(res => res.json())
-  .then(json => { fs.writeFileSync('../dist/events.yml', yaml.dump(json)) } )
+  .then(transformer)
+  .then(data => { fs.writeFileSync('../dist/events.yml', yaml.dump(data)) } )
--- src/main.gs
+++ src/main.gs
@@ -5,34 +5,9 @@ function doGet (e) {
   var sheet = spreadsheet.getSheetByName(sheetName)
   var data = jsonifySheet(sheet)
 
-  data = data.filter(function(row){
-    return row.info_check
-  }).reduce(function(acc, row){
-    acc[row.event_id] = row
-    delete row['event_id']
-    delete row['upload']
-    delete row['info_check']
-    Object.keys(row).forEach(function(key){
-      if(!row[key]) row[key] = null
-      if(key.match(/_id$/)) row[key] = semicolon_separated_list(row[key])
-      if(key === 'start' || key === 'end') row[key] = readableDate(row[key])
-    })
-    return acc
-  }, {})
-
   return ContentService
     .createTextOutput(JSON.stringify(data))
     .setMimeType(ContentService.MimeType.JSON)
-}
-
-function semicolon_separated_list(str){
-  if(!str) return str
-  return str.split(';').filter(function(s){ return s }).map(function(s){ return s.trim() })
-}
-
-function readableDate(datetime) {
-  if(!datetime) return datetime
-  return Utilities.formatDate(datetime, "GMT", "yyyy/MM/dd")
 }
 
 function jsonifySheet(sheet) {
